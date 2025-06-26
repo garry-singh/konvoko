ba6ef2adc73190a4767aba3edae8d19be51b1589
Garry <mrgarrysingh92@gmail.com>
2025-06-26 23:04:01 +1000

    Update incorrect env variable name

--- pages/api/clerk-webhook.ts
+++ pages/api/clerk-webhook.ts
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Supabase client (using service role)
 const supabase = createClient(
-  process.env.SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
