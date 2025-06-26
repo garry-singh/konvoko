22c7c78ca08f54a69efa9d6bdd19fd00a7a4d82c
Garry <mrgarrysingh92@gmail.com>
2025-06-26 22:51:34 +1000

    Update matcher to include clerk webhook

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ const isPublicRoute = createRouteMatcher([
     '/',
     '/sign-in(.*)',
     '/sign-up(.*)',
-    '/subscription(.*)'
+    '/subscription(.*)',
+    '/api/clerk-webhook',
   ])
 
 export default clerkMiddleware(async (auth, req) => {
